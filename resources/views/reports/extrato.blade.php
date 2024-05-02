3f1a2a1a9be7afd099e6644a2b22fca902f6885e
RandomPianist <reynoldscosta@hotmail.com>
2024-05-02 15:01:55 +0000

    Formatação de preço

--- resources/views/reports/extrato.blade.php
+++ resources/views/reports/extrato.blade.php
@@ -37,7 +37,7 @@
                         <h6 class = "pl-3 fw-600">{{ $produto["descr"] }}</h6>
                     </td>
                     <td width = "50%">
-                        <h6 class = "pl-3 fw-600">Preço: R$ {{ $produto["preco"] }}</h6>
+                        <h6 class = "pl-3 fw-600">Preço: R$ {{ str_replace(".", ",", $produto["preco"]) }}</h6>
                     </td>
                     <td style = "white-space:nowrap">
                         <h6 class = "pl-3 fw-600">Saldo total: {{ $produto["saldo"] }}</h6>
